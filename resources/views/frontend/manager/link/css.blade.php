18f7aae4d0703890d3e52d85849c47e4d65d1563
chauquangtam <chauquangtam2000@gmail.com>
2022-06-15 14:43:54 +0700

    up

--- resources/views/frontend/manager/link/css.blade.php
+++ resources/views/frontend/manager/link/css.blade.php
@@ -1,5 +1,6 @@
 <!-- ==== Favicons ==== -->
-<link rel="icon" href="{{url('frontend')}}/img/KCN.png" type="image/png">
+<link rel="icon" href="frontend/img/KCN.png" type="image/png">
+<!-- {{ url('public/frontend') }}/img/KCN.png -->
 <!-- ==== Google Font ==== -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700">
 
@@ -22,7 +23,7 @@
 <link rel="stylesheet" href="{{url('frontend/css/colors/theme-color-1.css')}}" id="changeColorScheme">
 
 <!-- ==== Custom Stylesheet ==== -->
-<link rel="stylesheet" href="{{url('frontend/css/custom.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/css/custom.css')}}">
 
 <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
 <!--[if lt IE 9]>
